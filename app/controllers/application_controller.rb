3de85b009daf2f893faf9f2f62f2cfd129568065
ashwinibavdane <ashwini.bavdane@mirraw.com>
2022-06-17 10:33:22 +0530

    Add static page controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
-
-  def hello
-    render plain: "hello, Ashwini!, Welcome to mirraw"
-  end
+   
 
 end
