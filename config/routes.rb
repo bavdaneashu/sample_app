683543649e3193e82172d1cdfa0f01a0291a1e3b
ashwinibavdane <ashwini.bavdane@mirraw.com>
2022-06-19 02:56:46 +0530

    SC

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get'static_pages/home'
+  
+  get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get  'static_pages/contact'
+  get 'users/new'
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
